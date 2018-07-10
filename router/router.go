1b8bc73770604f20ebb175fd248b834b0d64bcd8
Komei1009 <1994komei1009@gmail.com>
2018-07-10 11:39:26 +0900

    router追加

--- router/router.go
+++ router/router.go
@@ -9,6 +9,16 @@ import (
 func GetRouter() *gin.Engine {
 	r := gin.Default()
 	r.Use(cors)
+	
+	r.Static("/js", "./public/js")
+	r.Static("/image", "./public/image")
+	r.Static("/css", "./public/css")
+	
+	r.LoadHTMLGlob("view/*")
+	
+	r.NoRoute(func(c *gin.Context) {
+		c.HTML(200, "index.html", nil)
+	})
 
 	r.GET("/log", log)
 
