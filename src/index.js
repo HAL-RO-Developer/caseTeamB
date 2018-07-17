f9a3865bf2d25ef0819903231ce2936d2ca25094
HiyaNaka <nagaoka.hirotaka@gmail.com>
2018-07-17 09:48:36 +0900

    chengegoal追加

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import VueRouter from 'vue-router'
 import dashboad from './components/pages/dashboad.vue'
 import Login from './components/pages/login.vue'
 import Device from './components/pages/device.vue'
-import Records from './components/pages/records.vue'
-import SolvedList from './components/pages/solvedList.vue'
+import Details from './components/pages/goalDetails.vue'
 import Settings from './components/pages/settings.vue'
 import Children from './components/pages/children.vue'
 import Goals from './components/pages/goals.vue'
@@ -21,8 +20,7 @@ const routes = [
     { path: "/", component: dashboad },
     { path: "/login", component: Login },
     { path: "/device", component: Device },
-    { path: "/records/:filter", component: Records },
-    { path: "/records/:filter/:date", component: SolvedList },
+    { path: "/goals/details", component: Details },
     { path: "/settings", component: Settings },
     { path: "/children", component: Children },
     { path: "/goals", component: Goals },
